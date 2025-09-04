f85da6fbf02060d3c1c2a993a245425f981f7138
ayushhmaan <2023ci_ayushmanpathak_a@gmail.com>
2025-09-04 22:06:48 +0530

    GEMINI_CLONE DONE

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import "./SideBarr.css";
+import "./Sidebarr.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 
